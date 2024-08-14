0de4453da32c6fd0c503e5238aff343a7e6bbc01
Pufferfish404 <alpsxia56@gmail.com>
2024-08-14 11:44:47 -0400

    ran on pretrained model

--- YOLO setup/cardmodel.py
+++ YOLO setup/cardmodel.py
@@ -1,5 +1,5 @@
 from ultralytics import YOLO
 
-model = YOLO(r'C:\Users\wangm3\Documents\Celegans-object-detection\runs\detect\train\weights\best.pt')
+model = YOLO('yolov8m.pt')
 
 results = model.train(data=r"C:\Users\wangm3\Documents\Celegans-object-detection\Cards-3\data.yaml", epochs=20, imgsz=640, device="mps")
--- YOLO setup/liverun.py
+++ YOLO setup/liverun.py
@@ -1,6 +1,6 @@
 from ultralytics import YOLO
 
-model = YOLO('yolov8n.pt')
+model = YOLO(r'C:\Users\wangm3\Documents\Celegans-object-detection\runs\detect\Aug14_2024_Cards\weights\best.pt')
 
 #source can be a filepath or live camera (1)
-results = model(source=1, show=True, save=True)
+results = model(source=r"C:\Users\wangm3\Documents\Celegans-object-detection\pokerhands.jpg", show=True, save=True)
